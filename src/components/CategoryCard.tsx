de389baffa5a7b970058b71756db739211ef4ca1
JeremiasRy <ryttari.jeremias@gmail.com>
2022-12-24 10:00:56 +0200

    feat: category filtering

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../hooks/reduxHook";
+import { getCategory } from "../redux/reducers/categoryReducer";
 import ICategory from "../types/interfaces/category";
 
 export default function CategoryCard(props: {category: ICategory}) {
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
     return (
-        <div className="category-card" style={{backgroundImage: `url(${props.category.image})`}}>
+        <div 
+        className="category-card" 
+        onClick={() => {
+            dispatch(getCategory(props.category.id));
+            navigate("/products");
+        }} 
+        style={{backgroundImage: `url(${props.category.image})`}}>
             <div className="category-card__overlay"></div>
             <h2>{props.category.name}</h2>
         </div>
