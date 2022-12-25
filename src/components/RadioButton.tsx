48cb0c1cdfc6a73b36047c833615b7c7501eb8ac
JeremiasRy <ryttari.jeremias@gmail.com>
2022-12-25 12:20:26 +0200

    refactor: types for radio buttons

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -1,3 +1,3 @@
-export default function RadioButton(props: {label:string, value:string, setDirection:any, checked:boolean}) {
+export default function RadioButton(props: {label:string, value:"asc" | "desc", setDirection:React.Dispatch<React.SetStateAction<"desc" | "asc">>, checked:boolean}) {
     return <label><input type="radio" checked={props.checked} onChange={() => props.setDirection(props.value)}/>{props.label}</label>
 }
