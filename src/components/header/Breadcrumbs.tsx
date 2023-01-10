fbacad4dd1e98eaf992f7d6563786f2048f8b1e2
JeremiasRy <ryttari.jeremias@gmail.com>
2023-01-10 08:51:21 +0200

    reafactor: breadcrumbs

--- src/components/header/Breadcrumbs.tsx
+++ src/components/header/Breadcrumbs.tsx
@@ -5,18 +5,22 @@ export default function Breadcrumbs() {
     const paths = location.pathname.split("/");
     const filtered = paths.slice(0, paths.length - 1);
 
-    const Links = filtered.map(path => {
+    const Links:JSX.Element[] = filtered.map(path => {
         if (path === "") {
             return <Link to="/home" className="breadcrumb">Home</Link>
-        }
-        return <Link to={`/${path}`} className="breadcrumb">{path}</Link>
+        } else if (path === "create-product" || path === "true" || path === "false") {
+            return <></>;
+        } else {
+            let pathName = `${path[0].toLocaleUpperCase()}${path.slice(1)}`;
+            return <Link to={`/${path}`} className="breadcrumb">{pathName}</Link>
+        }    
     })
     if (paths[1] === "home" || paths[1] === "") {
         return <></>;
     }
     return (
         <div className="breadcrumbs">{Links.map(
-            (link, idx) => <div className="breadcrumb-wrap" key={link.toString() + idx}>&nbsp;/ {link}</div>
+            (link, idx) => <div className="breadcrumb-wrap" key={link.toString() + idx}>&nbsp; {link}</div>
             )}
         </div>
     );
